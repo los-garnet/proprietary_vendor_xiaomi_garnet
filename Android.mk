bf528a34081e649ddbcc1d9a898b6ffc220c17e4
Ramadhani <ramadhanifals@gmail.com>
2025-02-18 13:34:53 +0700

    garnet: Import firmware from V816.0.17.0.UNRMIXM
    Change-Id: I95c9d99b1e9ab3d41558c6c8ba472ad830fdf392

--- Android.mk
+++ Android.mk
@@ -3,3 +3,28 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),garnet)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,de868e1323deee69fcb2254cd0973a7fb7d3f7f7)
+$(call add-radio-file-sha1-checked,radio/aop.img,a8481edcf5a1a95f999de6f122861ded50a197e7)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,ec33f65585cb78f52bbfa5326c71d5342fb0f131)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,3641673095cbce71daaa0cf40191c435d12ff7f2)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,9fc24b18bc3a50216fa3deb6b9b5b6e5690d6727)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,c5548bd564817070da316667f59afa43bfc88307)
+$(call add-radio-file-sha1-checked,radio/dsp.img,9a5f44f9276c0d1d6b1c4ec79adb182777bdb112)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,b19e484c2f439e84f12e8637ada088bf42abf180)
+$(call add-radio-file-sha1-checked,radio/hyp.img,3fb57f7ccb95df9d513957a3813c7fefe492a21b)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,0fd6ecae71e3938a640939ebc246f0818e426c56)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,136e37aa5e64a787a0019678ff628f25d065f522)
+$(call add-radio-file-sha1-checked,radio/modem.img,848d3d7bf2bab1d323ca896ca077260830cd1d0e)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,b2c8f01e38755328d51ab3f56dc0991251303a2b)
+$(call add-radio-file-sha1-checked,radio/shrm.img,ffe57d4e7ed6b75e00ccdef30f7201f4952f7629)
+$(call add-radio-file-sha1-checked,radio/tz.img,1b974acc5720a33f49f2ed3b12644e4b599c5aba)
+$(call add-radio-file-sha1-checked,radio/uefi.img,ed737916a703fe01975b95a2ee1d15e794739aee)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,19c522155aca104aac7f8e4ede4f0959dbb3358f)
+$(call add-radio-file-sha1-checked,radio/xbl.img,fe3c9558c985f47dc15fdae9854933b3ee055e25)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,b50b33c4c5d131d5f9a31ed441c9b50797cd9053)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,0f94b23e94a6e981b3dee61c71777429f51bd0d2)
+
+endif
