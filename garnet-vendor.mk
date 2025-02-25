57655eb72392d9b833657e013d993bd5a7ecc2bd
Bruno Martins <bgcngm@gmail.com>
2025-02-25 10:14:16 +0700

    garnet: Declare IMS libs as symlinks during extraction
    Change-Id: I2a035323a483b8b2422b4a54219b2f6268ab4a61

--- garnet-vendor.mk
+++ garnet-vendor.mk
@@ -2108,6 +2108,8 @@ PRODUCT_PACKAGES += \
     wfdservice64
 
 PRODUCT_PACKAGES += \
+    system_ext_priv-app_ims_lib_arm64_libimscamera_jni_so \
+    system_ext_priv-app_ims_lib_arm64_libimsmedia_jni_so \
     vendor_lib_libEGL_adreno_so \
     vendor_lib_libGLESv2_adreno_so \
     vendor_lib_libq3dtools_adreno_so \
